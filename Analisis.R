33e9c8e5e4ef83867e6c5c7f8804fc7e33a9b2ee
Alberto Torreblanca <alberto.torreblanca@gmail.com>
2013-11-09 15:53:24 -0500

    Fuciones para utilizar en el Análisis de Teoría Clásica.

--- Analisis.R
+++ Analisis.R
@@ -1,7 +1,3 @@
-##### Paquetes a utilizar ######
-library("eRm", lib.loc="/home/albert1t0/R/x86_64-pc-linux-gnu-library/3.0") # Para análisis de Rasch
-library("CTT", lib.loc="/home/albert1t0/R/x86_64-pc-linux-gnu-library/3.0") # Para teoría clásica
-
 ##### Carga de datos ############
 
 # Forma 1
@@ -57,4 +53,97 @@ s4 <- merge(s4f1,s4f2, all.x=TRUE, all.y=TRUE)
 s4 <- merge(s4,s4f3, all.x=TRUE, all.y=TRUE)
 s4 <- merge(s4,s4f4, all.x=TRUE, all.y=TRUE)
 s4 <- merge(s4,s4f5, all.x=TRUE, all.y=TRUE)
+
+##### Paquetes a utilizar ######
+
+library("eRm", lib.loc="/home/albert1t0/R/x86_64-pc-linux-gnu-library/3.0") # Para análisis de Rasch
+library("CTT", lib.loc="/home/albert1t0/R/x86_64-pc-linux-gnu-library/3.0") # Para teoría clásica
+
+############ Funciones adicionales para el Análisis #################
+
+## Variante para poder guardar en excel los datos
+
+distractor.analysis2 <- function (items, key = NA, scores = NA, p.table = FALSE, write.csv = NA) 
+{
+  items <- as.data.frame(items)
+  if (length(key) == 1) 
+    key <- c(rep(key, ncol(items)))
+  if (missing(scores)) 
+    scores <- as.data.frame(score(items, key)$score)
+  if (missing(key)) 
+    warning("No se encontro clave")
+  else if (!length(key) == ncol(items)) {
+    warning("No se encontró clave o algunas claves han sido omitidas.")
+  }
+  score.level <- quantile(scores[, 1], c(0, 1/3, 2/3, 1))
+  score.level <- cut(scores[, 1], score.level, include.lowest = TRUE, 
+                     labels = c("bajo", "medio", "alto"))
+  itemtab <- function(response) {
+    xtabs(~response + score.level)
+  }
+  itemtabp <- function(response) {
+    round(prop.table(xtabs(~response + score.level), 2), 
+          3)
+  }
+  all.levels <- sort(unique(unlist(items)))
+  for (i in 1:ncol(items)) {
+    items[, i] <- factor(items[, i], levels = all.levels, 
+                         labels = ifelse(all.levels == key[i], paste("*", 
+                                                                     all.levels, sep = ""), paste(" ", all.levels, 
+                                                                                                  sep = "")))
+  }
+  out <- list()
+  if (p.table == FALSE) 
+    for (i in 1:ncol(items)) {
+      out[[i]] <- itemtab(items[, i])
+    }
+  else for (i in 1:ncol(items)) {
+    out[[i]] <- itemtabp(items[, i])
+  }
+  names(out) <- colnames(items)
+  if (!is.na(write.csv == "NA")) {
+    response <- ifelse(all.levels == key[i], paste("*", all.levels, 
+                                                   sep = ""), paste(" ", all.levels, sep = ""))
+    item <- c(rep(NA, length(all.levels)))
+    for (i in 1:ncol(items)) {
+      x <- as.data.frame(cbind(item, response, as.vector(out[[i]][, 
+                                                                  1]), as.vector(out[[i]][, 2]), as.vector(out[[i]][, 
+                                                                                                                    3])))
+      names(x) <- c(paste("item:", i), "respuestas", "bajo", 
+                    "medio", "alto")
+      suppressWarnings(write.table(x, write.csv, row.names = FALSE, 
+                                   na = " ", append = TRUE, sep=","))
+    }
+  }
+  names(out) <- paste("item:", c(seq(1:ncol(items))))
+  out
+}
+
+## Variante de Item análisis Teoría Clásica
+
+item.anal<-function(items, key = NA, name = NA)
+{
+  if(!is.na(name == NA )) {
+    name1<-paste(name,"_cnt.csv",sep="")
+    name2<-paste(name,"_prop.csv",sep="")
+    name3<-paste(name,"_itm.csv",sep="")
+  }
+  else { ## No se escribe archivos de distractores
+    name1 <- NA
+    name2 <- NA
+    name3 <- NA
+  }   
+  distractores1<-distractor.analysis2(items,as.character(key),write.csv = name1)
+  distractores2<-distractor.analysis2(items,as.character(key), p.table= TRUE, write.csv = name2)
+  puntajes<-score(items,as.character(key),output.scored=TRUE)
+  rel<-reliability(puntajes$scored)
+  item.table <- as.data.frame(round(cbind(rel$item.mean,rel$pbis,rel$alpha.if.deleted),digits = 4))
+  colnames(item.table)<-c("Dificultad","Corrrelación","AlphaExcluido")
+  rownames(item.table)<-paste("Item",1:rel$N_item,sep="_")
+  out <- list(Puntaje = rel$scale.mean, Puntaje.sd = rel$scale.sd, Personas = rel$N_person, Alpha = rel$alpha, Item = item.table, Distractores = distractores1, Distractores_Prop = distractores2, Calificacion = puntajes$score)
+  write.table(item.table,name3,sep=",")
+  class(out) <- "Análisis de items"
+  out
+}
+
 
